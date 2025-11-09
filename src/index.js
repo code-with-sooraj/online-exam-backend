6c9851eceea695460149faf0b9b6a1dba8dda8e6
code-with-sooraj <sauravuyadav003@gmail.com>
2025-11-09 20:16:35 +0530

    url fixed

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import crypto from 'crypto'
 dotenv.config()
 
 const app = express()
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN , credentials: true }))
 app.use(express.json({ limit: '1mb' }))
 
 app.get('/api/health', (req, res) => res.json({ ok: true }))
